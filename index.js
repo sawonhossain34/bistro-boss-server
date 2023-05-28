cbcd99dd470d7987e40cb43a6e7791b2f1c9d0d5
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-28 18:19:17 +0600

    cart database delete api create

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, BulkOperationBase } = require('mongodb');
+const { MongoClient, ServerApiVersion, BulkOperationBase, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -58,6 +58,13 @@ async function run() {
     app.post('/carts', async (req, res) => {
       const item = req.body;
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async (req,res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
